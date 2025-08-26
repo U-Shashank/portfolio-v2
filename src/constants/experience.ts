a7e1fc60a322aa207be6361dbb0bab737fb9922d
Shashank Upadhyay <shashank_@outlook.in>
2025-08-26 13:06:17 +0530

    hpe inter ove

--- src/constants/experience.ts
+++ src/constants/experience.ts
@@ -1,28 +1,28 @@
 const experiences = [
-    {
-        company: "Hewlett Packard Enterprise",
-        role: "Project Intern (HPE CPP)",
-        period: "January 2025 – Present",
-        highlights: [
-            "Working on system and service monitoring plugin, gaining hands-on experience in system monitoring.",
-            "Developing Nagios plugins to monitor system health and performance metrics.",
-            "Implementing data visualization techniques to present monitoring data effectively.",
-            "Collaborating with mentors to solve real-world technical challenges and improve problem-solving abilities.",
-            "Contributing to the development of monitoring solutions for enterprise systems.",
-        ],
-    },
-    {
-        company: "Advertyzement",
-        role: "Front-End Intern",
-        period: "December 2024 – March 2025",
-        highlights: [
-            "Developed a landing page, improving performance metrics from 50% to 90% through optimization and SEO.",
-            "Resolved critical bugs and implemented a cross-browser compatibility feature.",
-            "Collaborated with designers and backend developers to ensure seamless integration.",
-            "Implemented responsive design principles for optimal user experience across devices.",
-            "Participated in code reviews and contributed to improving code quality.",
-        ],
-    },
+  {
+    company: "Hewlett Packard Enterprise",
+    role: "Project Intern (HPE CPP)",
+    period: "February 2025 – July 2025",
+    highlights: [
+      "Working on system and service monitoring plugin, gaining hands-on experience in system monitoring.",
+      "Developing Nagios plugins to monitor system health and performance metrics.",
+      "Implementing data visualization techniques to present monitoring data effectively.",
+      "Collaborating with mentors to solve real-world technical challenges and improve problem-solving abilities.",
+      "Contributing to the development of monitoring solutions for enterprise systems.",
+    ],
+  },
+  {
+    company: "Advertyzement",
+    role: "Front-End Intern",
+    period: "December 2024 – March 2025",
+    highlights: [
+      "Developed a landing page, improving performance metrics from 50% to 90% through optimization and SEO.",
+      "Resolved critical bugs and implemented a cross-browser compatibility feature.",
+      "Collaborated with designers and backend developers to ensure seamless integration.",
+      "Implemented responsive design principles for optimal user experience across devices.",
+      "Participated in code reviews and contributed to improving code quality.",
+    ],
+  },
 ];
 
 export default experiences;
